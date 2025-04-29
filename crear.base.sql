ef50f112ecd17f3709ea3a85ebf9f0636408558e
SSIEETEE7 <tu_correo@ejemplo.com>
2025-04-29 16:55:58 -0600

    Reforzar cambio de función a index() y limpieza

--- crear.base.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS locatarios (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    nombre TEXT NOT NULL,
-    mercado TEXT NOT NULL,
-    productos TEXT NOT NULL,
-    telefono TEXT NOT NULL,
-    correo TEXT NOT NULL
-);
